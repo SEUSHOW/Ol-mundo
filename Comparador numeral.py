dd44ecc43c8c1d589ef8207949c8b97651bcea27
SEUSHOW <66682547+SEUSHOW@users.noreply.github.com>
2020-06-10 16:20:43 -0300

    Add files via upload

---
+++ Comparador numeral.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite um número para comparar com o segundo '))
+n2 = float(input('Digite o segundo número: '))
+
+if n1 > n2:
+     print('O número {} é maior '.format(n1))
+else:
+     print('O número {} é maior. '.format(n2))
