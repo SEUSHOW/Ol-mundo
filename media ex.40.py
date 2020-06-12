7e9f4f7e01b169e646cf9d64f08ce0c66473bc43
SEUSHOW <66682547+SEUSHOW@users.noreply.github.com>
2020-06-12 11:33:29 -0300

    Add files via upload

---
+++ media ex.40.py
@@ -0,0 +1,12 @@
+#Automatizando chinelada.
+
+nota = float(input('Digite sua nota: '))
+
+media = 7 <= nota
+
+if media:
+    print('Parabens meu filho voce foi aprovado.Por enquanto!')
+elif  5 <= nota and 6.9 >= nota:
+    print('voce esta de recuperaçao.Vai receber 10 chineladas.')
+else: 5 > nota
+print('Voce foi reprovado. Vai receber 20 chineladas.')
